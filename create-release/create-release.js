377b5e620f7bc0f6e8b331b480c33cc5eeda6b9f
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2022-11-08 10:58:39 +0000

    Add replacement for unmaintained actions/create-release

---
+++ create-release/create-release.js
@@ -0,0 +1,44 @@
+/* THIS FILE IS PART OF THE CYLC WORKFLOW ENGINE.
+Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+const {env} = process;
+const {writeFileSync} = require('fs');
+const {execSync} = require('cylc-action-utils');
+
+const cmd = [
+    'gh', 'release', 'create',
+    env.TAG,
+    `--repo '${env.REPO}'`,
+    `--target '${env.TARGET}'`,
+    `--title '${env.TITLE}'`,
+]
+
+if (env.BODY) {
+    cmd.push(`--notes '${env.BODY.replace(/'/g, "&apos;")}'`)
+} else if (env.BODY_PATH) {
+    cmd.push(`--notes-file '${env.BODY_PATH}'`)
+}
+if (env.IS_DRAFT === 'true') {
+    cmd.push('--draft')
+}
+if (env.IS_PRERELEASE === 'true') {
+    cmd.push('--prerelease')
+}
+
+const url = execSync(cmd.join(' ')).split('\n')[0]
+
+// Set output
+writeFileSync(env.GITHUB_OUTPUT, `html_url=${url}`, {flag: 'a'})
